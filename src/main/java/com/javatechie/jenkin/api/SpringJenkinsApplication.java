f43439f6b97c603991c7e0f7d6f668b23686e3b9
sudihits <sudivasishta@gmail.com>
2024-08-30 11:05:09 +0530

    added dot

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -12,7 +12,7 @@ public class SpringJenkinsApplication {
 
 	@GetMapping("/ping")
 	public String message() {
-		return "Wao!! Application Deployed successfully in SAP Cloud..";
+		return "Wao!! Application Deployed successfully in SAP Cloud...";
 	}
 
 	public static void main(String[] args) {
