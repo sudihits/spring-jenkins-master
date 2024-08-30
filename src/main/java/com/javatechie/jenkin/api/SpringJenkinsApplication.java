3cf08b8e88fbee69dbd7a25c3a632e0d7d793533
sudihits <sudivasishta@gmail.com>
2024-08-30 12:11:03 +0530

    introducing errors

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -11,7 +11,7 @@ public class SpringJenkinsApplication {
 
 
 	@GetMapping("/ping")
-	public String message() {
+	public  message() {
 		return "Wao!! Application Deployed successfully in SAP Cloud...";
 	}
 
